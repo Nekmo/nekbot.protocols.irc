c9f9b26595bf9f31f7e2b4804a811b40f92a0c05
Nekmo <contacto@nekmo.com>
2015-04-25 22:37:54 +0200

    Preparando Base para protocolo IRC. Añadidos métodos de envío de mensajes y descubrimiento del origen del mensaje (sala, privado...).

--- nekbot/protocols/irc/__init__.py
+++ nekbot/protocols/irc/__init__.py
@@ -1,66 +1,70 @@
 from collections import defaultdict
 from logging import getLogger
-from irc import bot
-from nekbot import settings
-from nekbot.protocols import Protocol
-from nekbot.utils.auth import AuthAddress, UserPassword
+
 import irc.bot
 import irc.strings
-from irc.client import ip_numstr_to_quad, ip_quad_to_numstr
+from irc.client import ip_numstr_to_quad
+
+from nekbot import settings
+from nekbot.protocols import Protocol
+from nekbot.protocols.irc.group_chat import GroupChatsIRC, GroupChatIRC
+from nekbot.protocols.irc.message import MessageIRC
+from nekbot.protocols.irc.utils import add_sharp
+from nekbot.utils.auth import AuthAddress
 
 
-"""A simple example bot.
-
-This is an example bot that uses the SingleServerIRCBot class from
-irc.bot.  The bot enters a channel and listens for commands in
-private messages and channel traffic.  Commands in channel messages
-are given by prefixing the text by the bot name followed by a colon.
-It also responds to DCC CHAT invitations and echos data sent in such
-sessions.
-
-The known commands are:
-
-    stats -- Prints some channel information.
-
-    disconnect -- Disconnect the bot.  The bot will try to reconnect
-                  after 60 seconds.
-
-    die -- Let the bot cease to exist.
-
-    dcc -- Let the bot invite you to a DCC CHAT connection.
+"""
 """
 
 __author__ = 'nekmo'
 
-
 logger = getLogger('nekbot.protocols.irc')
 
-def normalize_roomname(name):
-    if not name.startswith('#'):
-        return '#' + name
-    return name
+# event:
+# {'source': u'nekmo!~nekmo@localhost.localdomain',
+# 'type': 'pubmsg', 'target': u'#testing', 'arguments': [u'jugou']}
+#
+# channel:
+# {'username': 'NekBot', 'reactor': <irc.client.Reactor object at 0x7f44c2cf9d50>,
+# 'real_nickname': u'NekBot', 'handlers': {},
+# 'buffer': <irc.buffer.DecodingLineBuffer object at 0x7f44c2cf9e50>,
+# 'server_address': ('localhost', 6667), 'server': 'localhost',
+#  '_saved_connect': args_and_kwargs(args=('localhost', 6667, 'NekBot', None),
+# kwargs={'ircname': 'Nekbot Mirai IRC'}),
+#  'connect_factory': <irc.connection.Factory object at 0x7f44c2fb6490>,
+# 'socket': <socket._socketobject object at 0x7f44c5d118a0>, 'connected': True,
+#  'real_server_name': u'irc.example.net', 'password': None, 'nickname': 'NekBot',
+#  'port': 6667, 'ircname': 'Nekbot Mirai IRC',
+# 'features': <irc.features.FeatureSet object at 0x7f44c2cf9e10>}
+
 
 
 class ServerBot(irc.bot.SingleServerIRCBot):
-    def __init__(self, groupchats_list, username, realname, server, port=6667):
+    def __init__(self, protocol, groupchats_list, username, realname, server, port=6667):
+        self.groupchats = {} # {'groupchat': <Groupchat object>}
         self.groupchats_list = groupchats_list
+        self.protocol = protocol
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port)], username, realname)
 
-    def input_message(self, channel, event):
-        MessageIRC(self, msg)
+    def input_message(self, event):
+        self.protocol.propagate('message', MessageIRC(self, event))
 
     def on_nicknameinuse(self, channel, event):
         channel.nick(channel.get_nickname() + "_")
 
-    def on_welcome(self, channel, event):
+    def on_welcome(self, connection, event):
         for groupchat in self.groupchats_list:
-            channel.join(groupchat)
+            self.join_groupchat(add_sharp(groupchat))
 
-    def on_privmsg(self, channel, event):
-        self.input_message()
+    def on_privmsg(self, connection, event):
+        self.input_message(event)
         # self.do_command(e, e.arguments[0])
 
-    def on_pubmsg(self, channel, event):
+    def on_pubmsg(self, connection, event):
+        # self.send_pubmsg('#testing', 'Message')
+        # connection.privmsg('Nekmo', 'Test')  # Mensaje privado
+        # self.connection.privmsg('#testing', 'Test')
+        self.input_message(event)
         a = event.arguments[0].split(":", 1)
         if len(a) > 1 and irc.strings.lower(a[0]) == irc.strings.lower(self.connection.get_nickname()):
             pass
@@ -71,6 +75,14 @@ class ServerBot(irc.bot.SingleServerIRCBot):
         # non-chat DCC messages are raw bytes; decode as text
         text = event.arguments[0].decode('utf-8')
         channel.privmsg("You said: " + text)
+
+    def join_groupchat(self, channel, key=''):
+        self.connection.join(channel, key)
+
+    def on_join(self, connection, event):
+        groupchat = GroupChatIRC(self, self.channels[event.target], event.target, event.source)
+        self.groupchats[str(groupchat)] = groupchat
+        self.protocol.groupchats[str(groupchat)] = groupchat
 
     def on_dccchat(self, channel, event):
         if len(event.arguments) != 2:
@@ -114,6 +126,7 @@ class ServerBot(irc.bot.SingleServerIRCBot):
 class Irc(Protocol):
     def __init__(self, nekbot):
         self.servers = []
+        self.groupchats = GroupChatsIRC(self)
         self._addresses = [AuthAddress(addr) for addr in settings.IRC_GROUPCHATS]  # groupchat@server
         self._groupchats_by_server = defaultdict(list)  # {server: [groupchat@server]}
         super(Irc, self).__init__(nekbot)
@@ -126,7 +139,8 @@ class Irc(Protocol):
             # clase abstraida para obtener user@host, donde user en realidad es el groupchat.
             server = AuthAddress(server)
             serverbot = ServerBot(
-                [normalize_roomname(addr.user) for addr in self._groupchats_by_server[str(server)]],  # rooms
+                self,  # protocol instance
+                [addr.user for addr in self._groupchats_by_server[str(server)]],  # rooms
                 auth['username'],  # username
                 auth.get('realname', auth['username']),  # Realname
                 server.host,  # hostname without port
--- nekbot/protocols/irc/message.py
+++ nekbot/protocols/irc/message.py
@@ -1,7 +1,35 @@
 from nekbot.protocols import Message
+from nekbot.protocols.irc.utils import remove_sharp
+from nekbot.protocols.irc.user import UserIRC
 
 __author__ = 'nekmo'
 
+
 class MessageIRC(Message):
-    def __init__(self, channel, event):
-        pass
+    def __init__(self, server, event):
+        self.server = server
+        self.event = event
+        body = event.arguments[0]
+        if self.is_groupchat:
+            groupchat = server.groupchats[remove_sharp(event.target)]
+        else:
+            groupchat = None
+        user = UserIRC(server, event.source)
+        super(MessageIRC, self).__init__(server.protocol, body, user, groupchat)
+
+    @property
+    def is_groupchat(self):
+        if self.is_private:
+            return False
+        elif self.is_public:
+            return True
+        else:
+            raise NotImplementedError('Improperly implementation of is_groupchat.')
+
+    @property
+    def is_private(self):
+        return self.event.type == 'privmsg'
+
+    @property
+    def is_public(self):
+        return self.event.type == 'pubmsg'
